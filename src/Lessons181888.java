a69c4ba6272ab9727d3e5dc7b94b432f3262d577
Papapapaori <scans2001@gmail.com>
2024-01-02 00:39:11 +0900

    둘째날

---
+++ src/Lessons181888.java
@@ -0,0 +1,10 @@
+public class Lessons181888 {
+
+    public static int[] solution(int[] num_list, int n) {
+        int[] answer = new int[(int)Math.ceil((double)num_list.length/n)];
+        for(int i = 0; i < answer.length; i++)
+            answer[i] = num_list[i*n];
+        return answer;
+    }
+
+}
---
+++ src/Lessons181907.java
@@ -0,0 +1,6 @@
+public class Lessons181907 {
+    public String solution(String my_string, int n) {
+
+        return my_string.substring(0, n);
+    }
+}
